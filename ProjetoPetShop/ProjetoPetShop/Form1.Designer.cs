50a6a06b6ca6c78f4a36d5890fb8fdbdf6177aab
Gustavo Vicente <104469563+Gustavovicent@users.noreply.github.com>
2024-05-12 09:36:06 -0300

    Add files via upload
    Desenvolvimento de um projeto de gerenciamento de um Pet Shop com cadastros de clientes e animais agendamento de consultas e aos poucos estou adicionando mais funcionalidades

---
+++ ProjetoPetShop/ProjetoPetShop/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ProjetoPetShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelTopo = new System.Windows.Forms.Panel();
+            this.panelLateral = new System.Windows.Forms.Panel();
+            this.btnAgendamentos = new System.Windows.Forms.Button();
+            this.btnServicos = new System.Windows.Forms.Button();
+            this.btnAnimais = new System.Windows.Forms.Button();
+            this.btnRaca = new System.Windows.Forms.Button();
+            this.btnCliente = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelSelecao = new System.Windows.Forms.Panel();
+            this.panelCentral = new System.Windows.Forms.Panel();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.panelTopo.SuspendLayout();
+            this.panelLateral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTopo
+            // 
+            this.panelTopo.BackColor = System.Drawing.Color.OliveDrab;
+            this.panelTopo.Controls.Add(this.btnSair);
+            this.panelTopo.Controls.Add(this.btnMin);
+            this.panelTopo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTopo.Location = new System.Drawing.Point(0, 0);
+            this.panelTopo.Name = "panelTopo";
+            this.panelTopo.Size = new System.Drawing.Size(1253, 58);
+            this.panelTopo.TabIndex = 0;
+            // 
+            // panelLateral
+            // 
+            this.panelLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panelLateral.Controls.Add(this.panelSelecao);
+            this.panelLateral.Controls.Add(this.panel1);
+            this.panelLateral.Controls.Add(this.btnInicio);
+            this.panelLateral.Controls.Add(this.btnAgendamentos);
+            this.panelLateral.Controls.Add(this.btnServicos);
+            this.panelLateral.Controls.Add(this.btnAnimais);
+            this.panelLateral.Controls.Add(this.btnRaca);
+            this.panelLateral.Controls.Add(this.btnCliente);
+            this.panelLateral.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLateral.Location = new System.Drawing.Point(0, 58);
+            this.panelLateral.Name = "panelLateral";
+            this.panelLateral.Size = new System.Drawing.Size(317, 602);
+            this.panelLateral.TabIndex = 1;
+            this.panelLateral.Paint += new System.Windows.Forms.PaintEventHandler(this.panelLateral_Paint);
+            // 
+            // btnAgendamentos
+            // 
+            this.btnAgendamentos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAgendamentos.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgendamentos.Location = new System.Drawing.Point(31, 518);
+            this.btnAgendamentos.Name = "btnAgendamentos";
+            this.btnAgendamentos.Size = new System.Drawing.Size(258, 61);
+            this.btnAgendamentos.TabIndex = 4;
+            this.btnAgendamentos.Text = "Agendadmentos";
+            this.btnAgendamentos.UseVisualStyleBackColor = true;
+            // 
+            // btnServicos
+            // 
+            this.btnServicos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnServicos.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnServicos.Location = new System.Drawing.Point(31, 413);
+            this.btnServicos.Name = "btnServicos";
+            this.btnServicos.Size = new System.Drawing.Size(258, 61);
+            this.btnServicos.TabIndex = 3;
+            this.btnServicos.Text = "Serviços";
+            this.btnServicos.UseVisualStyleBackColor = true;
+            // 
+            // btnAnimais
+            // 
+            this.btnAnimais.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnimais.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnimais.Location = new System.Drawing.Point(31, 314);
+            this.btnAnimais.Name = "btnAnimais";
+            this.btnAnimais.Size = new System.Drawing.Size(258, 61);
+            this.btnAnimais.TabIndex = 2;
+            this.btnAnimais.Text = "Animais";
+            this.btnAnimais.UseVisualStyleBackColor = true;
+            this.btnAnimais.Click += new System.EventHandler(this.btnAnimais_Click);
+            // 
+            // btnRaca
+            // 
+            this.btnRaca.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRaca.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRaca.Location = new System.Drawing.Point(31, 225);
+            this.btnRaca.Name = "btnRaca";
+            this.btnRaca.Size = new System.Drawing.Size(258, 61);
+            this.btnRaca.TabIndex = 1;
+            this.btnRaca.Text = "Raça";
+            this.btnRaca.UseVisualStyleBackColor = true;
+            // 
+            // btnCliente
+            // 
+            this.btnCliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCliente.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCliente.Location = new System.Drawing.Point(31, 131);
+            this.btnCliente.Name = "btnCliente";
+            this.btnCliente.Size = new System.Drawing.Size(258, 61);
+            this.btnCliente.TabIndex = 0;
+            this.btnCliente.Text = "Clientes";
+            this.btnCliente.UseVisualStyleBackColor = true;
+            this.btnCliente.Click += new System.EventHandler(this.btnCliente_Click);
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Location = new System.Drawing.Point(31, 35);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(260, 53);
+            this.btnInicio.TabIndex = 5;
+            this.btnInicio.Text = "Início";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(-174, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(168, 25);
+            this.panel1.TabIndex = 6;
+            // 
+            // panelSelecao
+            // 
+            this.panelSelecao.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panelSelecao.Location = new System.Drawing.Point(3, 131);
+            this.panelSelecao.Name = "panelSelecao";
+            this.panelSelecao.Size = new System.Drawing.Size(23, 61);
+            this.panelSelecao.TabIndex = 2;
+            // 
+            // panelCentral
+            // 
+            this.panelCentral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panelCentral.BackgroundImage = global::ProjetoPetShop.Properties.Resources.luups;
+            this.panelCentral.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelCentral.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelCentral.Location = new System.Drawing.Point(317, 58);
+            this.panelCentral.Name = "panelCentral";
+            this.panelCentral.Size = new System.Drawing.Size(936, 602);
+            this.panelCentral.TabIndex = 2;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSair.BackgroundImage")));
+            this.btnSair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Location = new System.Drawing.Point(1203, 13);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(38, 39);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnMin
+            // 
+            this.btnMin.BackgroundImage = global::ProjetoPetShop.Properties.Resources._786263;
+            this.btnMin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMin.Location = new System.Drawing.Point(1147, 12);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(50, 39);
+            this.btnMin.TabIndex = 1;
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 33F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 660);
+            this.Controls.Add(this.panelCentral);
+            this.Controls.Add(this.panelLateral);
+            this.Controls.Add(this.panelTopo);
+            this.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panelTopo.ResumeLayout(false);
+            this.panelLateral.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTopo;
+        private System.Windows.Forms.Panel panelLateral;
+        private System.Windows.Forms.Panel panelCentral;
+        private System.Windows.Forms.Button btnAgendamentos;
+        private System.Windows.Forms.Button btnServicos;
+        private System.Windows.Forms.Button btnAnimais;
+        private System.Windows.Forms.Button btnRaca;
+        private System.Windows.Forms.Button btnCliente;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelSelecao;
+    }
+}
+
---
+++ ProjetoPetShop/ProjetoPetShop/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPetShop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panelLateral_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnimais_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+
+            //pergunta se realmente quer fechar
+
+           if (MessageBox.Show("Deseja mesmo sair ","Super-Cão",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();//Botão Fechar
+            }
+                    
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+
+            //Botão Minimizar
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            panelCentral.Controls.Clear();
+
+            panelSelecao.Top = btnInicio.Top;
+
+            
+
+
+
+        }
+
+        private void btnCliente_Click(object sender, EventArgs e)
+        {
+            frmCadCliente cliente = new frmCadCliente();
+            cliente.TopLevel = false;// não é painel de topo
+            cliente.Dock = DockStyle.Fill;//configura o Dock paraabrir a imagem certa no painel
+            panelCentral.Controls.Clear();//limpa o painel para não ficar uma aba aberta em cima da outra
+            panelCentral.Controls.Add(cliente);//chama o formulario cliente
+            cliente.Show();
+
+            //Aplica a posição do eixo X do botão ao panelSeleção
+
+            panelSelecao.Top = btnCliente.Top;
+        }
+    }
+}
